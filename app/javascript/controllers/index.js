913ee35e92444268c39fa128188048803faa99c9
Carl Mercier <carl@carlmercier.com>
2023-11-30 20:27:48 -0500

    added stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,6 +6,9 @@ application.register("hello", HelloController)
 import ExampleController from "./example_controller"
 application.register('example', ExampleController)
 
+import FoobarController from "./foobar_controller"
+application.register('foobar', FoobarController)
+
 import Flatpickr from "stimulus-flatpickr"
 require("flatpickr/dist/flatpickr.css")
 application.register('flatpickr', Flatpickr)
